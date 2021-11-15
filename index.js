0a332adbd5bb0f8fd88359414e583ffb6afde50c
Loovelllyy <sedykh.k.n@gmail.com>
2021-11-15 13:17:28 +0300

    update js

--- index.js
+++ index.js
@@ -8,8 +8,8 @@ const random = (min, max) => {
 
 containerDesk.addEventListener("click", () => {
     let circleWidth = random(10, 50);
-    circle.style.width = `${circleWidth}px`;
-    circle.style.height = `${circleWidth}px`;
+    // circle.style.width = `${circleWidth}px`;
+    // circle.style.height = `${circleWidth}px`;
     circle.style.background = `rgb(${random(0, 256)}, ${random(0, 256)}, ${random(0, 256)})`;
     circle.style.left = `${random(0, 500 - circleWidth)}px`;
     circle.style.top = `${random(0, 300 - circleWidth)}px`
