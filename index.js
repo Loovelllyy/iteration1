16c06b53fb05fad02e9859fa13bea111650e6531
Loovelllyy <sedykh.k.n@gmail.com>
2021-11-17 18:04:18 +0300

    made 4 iteration

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 const containerDesk = document.getElementsByClassName("container-desk")[0];
 const circle = document.getElementsByClassName("circle")[0];
-
+const operands = document.querySelectorAll(".operand");
+const btnResult = document.querySelector(".btn-result");
+const resultAddition = document.querySelector(".result-addition");
 
 const random = (min, max) => {
   return Math.floor(Math.random() * (max - min + 1)) + min;
@@ -14,3 +16,19 @@ containerDesk.addEventListener("click", () => {
     circle.style.left = `${random(0, 500 - circleWidth)}px`;
     circle.style.top = `${random(0, 300 - circleWidth)}px`
 })
+
+
+btnResult.addEventListener("click", () => {
+    try {
+        console.log("An error has occurred")
+        resultAddition.textContent = res;
+    } catch (er) {
+        alert(`${er.message}, we are trying to fix it `)
+        let res = 0;
+        operands.forEach(el => {
+            res += Number(el.value);
+        })
+        resultAddition.textContent = res;
+    }
+})
+
